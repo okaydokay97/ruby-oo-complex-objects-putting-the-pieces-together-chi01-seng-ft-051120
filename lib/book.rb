bf9d27d46f6bd8f9086f21a0aa9c09e192fa7f0f
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-05-13 13:12:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,35 @@
 class Book
-  
+ 
   def initialize(title)
     @title = title
   end
-  
-  def title 
+ 
+  def title
     @title
   end
-  
+ 
   def author=(author)
     @author = author
   end
-  
-  def author 
+ 
+  def author
     @author
   end
-  
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
 end
-
